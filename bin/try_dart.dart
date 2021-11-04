be178c424f52061319e47fd2b6f53402ab011e56
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-11-04 14:32:19 +0700

    try dart

--- bin/try_dart.dart
+++ bin/try_dart.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 main(List<String> arguments) {
+  print("Please Input the number:");
   String? input = stdin.readLineSync();
   double? number = double.tryParse(input!);
 
-  print("Hello world!");
   print(number!+12);
 }
