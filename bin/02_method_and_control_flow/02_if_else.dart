c8ac641139723b101a1ffa719ee5af75b559f417
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-11-17 17:30:38 +0700

    if else

---
+++ bin/02_method_and_control_flow/02_if_else.dart
@@ -0,0 +1,13 @@
+void main() {
+  int angka = 7;
+
+  if (angka == 0) {
+    print("nol");
+  } else {
+    if (angka % 2 == 0) {
+      print("Genap");
+    } else {
+      print("Ganjil");
+    }
+  }
+}
