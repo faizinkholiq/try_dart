4d1b5d847bf74aba2675a40969a159436aa7ec6a
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-11-17 15:21:08 +0700

    complete first challenge

--- bin/01_tipe_data_dan_operator/challenges/01_challenge.dart
+++ bin/01_tipe_data_dan_operator/challenges/01_challenge.dart
@@ -1,28 +1,29 @@
 void main() {
+
   String name = "Faizin Seafood";
   int year = 2000;
   String owner = "Nasrun Faizin Kholiq";
   String address = "Jl. Amat Wakhid no 10, Bantul";
   int phone = 089674344414;
-  bool status = true;
-  List<Map<String, dynamic>> foods = [
+  bool open = true;
+  List<Map> foods = [
     {"name": "Kepiting Rebus", "price": 40000},
     {"name": "Nasi Goreng", "price": 20000},
     {"name": "Udang Asam Manis", "price": 50000},
     {"name": "Sate Cumi", "price": 30000},
   ];
-  List<Map<String, dynamic>> drinks = [
+  List<Map> drinks = [
     {"name": "Es Jeruk", "price": 5000},
     {"name": "Es Kelapa", "price": 10000},
     {"name": "Es Teh", "price": 3000},
   ];
 
-  Map<String, dynamic> resto = {
+  Map resto = {
     "name": name,
     "year": year,
     "owner": owner,
     "address": address,
-    "status": status,
+    "open": open,
     "phone": phone,
     "foods": foods,
     "drinks": drinks
