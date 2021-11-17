9eb0de68c8260e2201509ede37008be8ba61694d
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-11-17 18:40:29 +0700

    loop

---
+++ bin/02_method_and_control_flow/05_for.dart
@@ -0,0 +1,11 @@
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    print(i);
+  }
+
+  // Looping on list
+  List foods = ["Sate", "Nasi Goreng", "Bakse"];
+  for (int i = 0; i < foods.length; i++) {
+    print(foods[i]);
+  }
+}
