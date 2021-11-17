64e39d9fb2fb07a2ed5c94a72d6f1925d6ed59a0
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-11-17 15:31:32 +0700

    complete second challenge

---
+++ bin/01_tipe_data_dan_operator/challenges/02_challenge.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Keliling Persegi
+  int s = 4;
+  int kp = 4 * s;
+  print(kp);
+
+  // Luas Lingkaran
+  double phi = 3.14;
+  double r = 10;
+  double ll = phi * (r * r);
+  print(ll);
+
+  // Volume Balok
+  int p = 8;
+  int l = 4;
+  int t = 4;
+  int vb = p * l * t;
+  print(vb);
+}
