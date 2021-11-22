81dbd60438bb1ba1314faef36f600531187e50fa
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-11-22 10:19:51 +0700

    setter & getter;

--- bin/03_class_and_object/01_properties_and_methods.dart
+++ bin/03_class_and_object/01_properties_and_methods.dart
@@ -1,14 +1,32 @@
 void main() {
-  RekeningBang rekeningBang = new RekeningBang();
+  RekeningBang rekeningFaiz = new RekeningBang();
 
-  rekeningBang.owner = "faizin";
-  rekeningBang.bank = "BCF - Bank Central Faizin";
-  rekeningBang.saldo = 1000000000000;
+  rekeningFaiz.owner = "faizin";
+  rekeningFaiz.bank = "BCF - Bank Central Faizin";
+  rekeningFaiz.saldo = 1000000000000;
 
-  print(rekeningBang.owner);
-  print(rekeningBang.bank);
-  
-  rekeningBang.cekSaldo();
+  print(rekeningFaiz.owner);
+  print(rekeningFaiz.bank);
+
+  rekeningFaiz.cekSaldo();
+  print("--------------------------");
+
+  RekeningBang rekeningKholiq = new RekeningBang(
+    owner: "kholiq",
+    bank: "BNK - Bank Nasional Kholiq",
+    saldo: 25000000,
+  );
+
+  rekeningKholiq.cekSaldo();
+  print("--------------------------");
+
+  RekeningBang rekeningNasrun = new RekeningBang(
+    owner: "nasrun",
+    bank: "BNK - Bank Rakyat Nasrun",
+  );
+
+  rekeningNasrun.setSaldo = 1460000000;
+  print(rekeningNasrun.getSaldo);
 }
 
 class RekeningBang {
@@ -16,7 +34,20 @@ class RekeningBang {
   String? bank;
   int? saldo;
 
+  // Construct
+  RekeningBang({this.owner, this.bank, this.saldo});
+
+  // Function
   cekSaldo() {
     print("saldo saat ini $saldo");
+  }
+
+  // Setter & Getter
+  set setSaldo(int saldo) {
+    this.saldo = saldo;
+  }
+
+  int? get getSaldo {
+    return saldo;
   }
 }
