becd8653134f87a8226390ff63b93464cef89bd5
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-01-21 03:27:12 +0700

    arrow function

--- bin/02_method_and_control_flow/01_method_function.dart
+++ bin/02_method_and_control_flow/01_method_function.dart
@@ -5,6 +5,10 @@ void main() {
   int sisi = 10;
   int volume = volumeBalok(sisi);
   print(volume);
+
+  perkenalan("hazard");
+  print(volumeKubus(2));
+  print('phi ${nilaiPhi()}');
 }
 
 void showTitle(String title) {
@@ -14,3 +18,11 @@ void showTitle(String title) {
 int volumeBalok(int sisi) {
   return sisi * sisi * sisi;
 }
+
+// Arrow function
+
+void perkenalan(String nama) => print("Halo salam kenal! I am $nama");
+
+int volumeKubus(int sisi) => sisi * sisi * sisi;
+
+double nilaiPhi() => 3.14;
