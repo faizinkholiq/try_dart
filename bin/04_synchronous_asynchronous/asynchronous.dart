cac4ab23207b37b264cbd999fc08cb9c2c73f403
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-01-25 16:33:33 +0700

    synchronous & asynchronous

---
+++ bin/04_synchronous_asynchronous/asynchronous.dart
@@ -0,0 +1,13 @@
+import 'synchronous.dart';
+
+void main() {
+  fetchData();
+  print("menunggu data");
+}
+
+Future<void> fetchData() {
+  return Future.delayed(
+    Duration(seconds: 2),
+    () => print("fetch data"),
+  );
+}
